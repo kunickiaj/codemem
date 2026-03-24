c9070e27f2a3aed502c75cb8d172b80bf30e73df
Adam Kunicki <kunickiaj@gmail.com>
2026-03-23 17:04:37 -0700

    chore(release): bump packages to 0.20.0-alpha.7

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -3,6 +3,6 @@ import { VERSION } from "./index.js";
 
 describe("core", () => {
 	it("exports a version string", () => {
-		expect(VERSION).toBe("0.20.0-alpha.6");
+		expect(VERSION).toBe("0.20.0-alpha.7");
 	});
 });
